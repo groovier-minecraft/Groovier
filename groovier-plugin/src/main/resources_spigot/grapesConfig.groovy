f0190c79a87381b89d50f6a39dbbae25a28fd438
Eric Lam <tsukiko852@gmail.com>
2023-01-17 04:26:47 +0000

    updated spigot / bungee version from grapesConfig.groovy

--- groovier-plugin/src/main/resources_spigot/grapesConfig.groovy
+++ groovier-plugin/src/main/resources_spigot/grapesConfig.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name = 'spigot-repo', root = 'https://hub.spigotmc.org/nexus/content/repositories/snapshots/')
-@Grab(group = 'org.spigotmc', module = 'spigot-api', version = '1.19-R0.1-SNAPSHOT')
+@Grab(group = 'org.spigotmc', module = 'spigot-api', version = '1.19.3-R0.1-SNAPSHOT')
 @GrabResolver(name = 'groovier', root = 'https://xuancat.app:8443/repository/java/', m2Compatible = true)
 @Grab(group = 'org.groovier', module = 'groovier-scripts', version = '0.0.1')
 import java.lang.Object
