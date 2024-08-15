1a1ea23fcf6a6598c89a2dd38939e8c3530c5cb3
ericlam <ericlam@hket.com>
2024-08-15 16:37:44 +0800

    removed xuan repository and change to use github repository

--- groovier-plugin/src/main/resources_spigot/grapesConfig.groovy
+++ groovier-plugin/src/main/resources_spigot/grapesConfig.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name = 'spigot-repo', root = 'https://hub.spigotmc.org/nexus/content/repositories/snapshots/')
-@Grab(group = 'org.spigotmc', module = 'spigot-api', version = '1.19.3-R0.1-SNAPSHOT')
-@GrabResolver(name = 'groovier', root = 'https://xuancat.app:8443/repository/java/', m2Compatible = true)
-@Grab(group = 'org.groovier', module = 'groovier-scripts', version = '0.0.1')
+@Grab(group = 'org.spigotmc', module = 'spigot-api', version = '1.20.4-R0.1-SNAPSHOT')
+@GrabResolver(name = 'groovier', root = 'https://maven.pkg.github.com/groovier-minecraft/groovier')
+@Grab(group = 'org.groovier', module = 'groovier-scripts', version = '0.0.4')
 import java.lang.Object
