4f2128bdd49ac3879467c430524f4d13e65a9461
ericlam <ericlam@hket.com>
2024-11-11 10:52:38 +0800

    upgraded for v1.21 spigot

--- groovier-plugin/src/main/resources_bungee/grapesConfig.groovy
+++ groovier-plugin/src/main/resources_bungee/grapesConfig.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name = 'bungee-repo', root = 'https://oss.sonatype.org/content/repositories/snapshots')
-@Grab(group = 'net.md-5', module = 'bungeecord-api', version = '1.21-R0.1-SNAPSHOT')
+@Grab(group = 'net.md-5', module = 'bungeecord-api', version = '1.20-R0.1')
 @GrabResolver(name = 'groovier', root = 'https://maven.pkg.github.com/groovier-minecraft/groovier')
 @Grab(group = 'org.groovier', module = 'groovier-scripts', version = '0.0.4')
 import java.lang.Object
--- groovier-plugin/src/main/resources_spigot/grapesConfig.groovy
+++ groovier-plugin/src/main/resources_spigot/grapesConfig.groovy
@@ -1,5 +1,5 @@
 @GrabResolver(name = 'spigot-repo', root = 'https://hub.spigotmc.org/nexus/content/repositories/snapshots/')
-@Grab(group = 'org.spigotmc', module = 'spigot-api', version = '1.20.4-R0.1-SNAPSHOT')
+@Grab(group = 'org.spigotmc', module = 'spigot-api', version = '1.21.1-R0.1-SNAPSHOT')
 @GrabResolver(name = 'groovier', root = 'https://maven.pkg.github.com/groovier-minecraft/groovier')
 @Grab(group = 'org.groovier', module = 'groovier-scripts', version = '0.0.4')
 import java.lang.Object
